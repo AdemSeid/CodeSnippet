463fc3560278870836cf74d933e987093fbf4b9b
AdemSeid <119794583+AdemSeid@users.noreply.github.com>
2023-09-12 03:21:29 +0300

    Update check if the given path contain_path_or_other_file.py

--- check if the given path contain_path_or_other_file.py
+++ check if the given path contain_path_or_other_file.py
@@ -1,6 +1,5 @@
+
 import os
-
-
 
 def list_folders(path):
     has_subfolders = False  # Initialize a flag to check if the folder has subfolders
@@ -12,7 +11,6 @@ def list_folders(path):
             has_subfolders = True  # Set the flag to True if subfolders are found
     
     if not has_subfolders:
-        # Print the path and name of the folder if it doesn't contain subfolders
         print("Folder Path:", path)
         print("Folder Name:", os.path.basename(path))
 
@@ -24,47 +22,4 @@ if os.path.exists(path_to_fldr) and os.path.isdir(path_to_fldr):
     list_folders(path_to_fldr)
 else:
     print("The specified path does not exist or is not a directory.")
-
-
-var = "Hel"
-print(f'hi{var}')
-
-
-'''
-import os
-
-# Define the folder name stored in the 'folder_name' variable
-folder_name = "animal"
-
-# Create the full folder name with "result" appended
-full_folder_name = folder_name + " result"
-
-# Initialize a counter to append to the folder name if needed
-counter = 1
-new_folder_name = full_folder_name
-
-# Check if the folder already exists
-while os.path.exists(new_folder_name):
-    # If it does, add a number inside parentheses and check again
-    counter += 1
-    new_folder_name = f"{full_folder_name}({counter})"
-
-# Create the final folder
-os.makedirs(new_folder_name)
-
-# Your code to save the file in the folder
-# Replace this line with your code to save the file
-# For example, you can use shutil.copy() or any other method to save the file.
-# Here's an example of how you can copy a file to the created folder:
-import shutil
-file_to_save = "your_file.txt"  # Replace with the actual file you want to save
-shutil.copy(file_to_save, os.path.join(new_folder_name, os.path.basename(file_to_save)))
-
-# Now, the file will be saved in the folder with the appropriate name (e.g., "animal result(1)").
-
-
-'''
-
-
-
 
